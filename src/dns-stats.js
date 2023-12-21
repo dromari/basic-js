32cd1e6288e1d438d1513860bd86947ad9a47d82
dromari <7709722@gmail.com>
2023-12-21 12:00:23 +0300

    feat: part of task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let obj = {};
+
+  for (let i = 0; i < domains.length; i++) {
+    let a = domains[i].split('.').reverse();
+    let b = '';
+
+    for (let j = 0; j < a.length; j++) {
+      b += '.' + a[j];
+      obj[b] = (obj[b] || 0) + 1;
+    }
+  }
+
+  return obj;
 }
 
 module.exports = {
